93ec3da539f38cdcc6a3706bbb20af5049d7ac2a
invek <invekkr@gmail.com>
2025-03-15 16:03:26 +0530

    math

--- Math/countDivisiors.cpp
+++ Math/countDivisiors.cpp
@@ -1,22 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n=4;
    
-    int num = 1,ans=0;
-    
-    for(int a=1;a<=num;a++){
-        int sum = 0;
-    for(int i=1;i<=sqrt(a);i++){
-       if(a%i==0){
-        if(i*i==a)  sum = sum + i;
-        else{
-            sum = sum+i;
-            sum = sum+a/i;
-        }
-       }
+    int ans = 0;
+    for(int i=1;i<=n;i++){
+        ans += i*(n/i);
     }
-    ans = ans+ sum;
-}
-        cout<<ans;
+    return ans;
+        cout<<"ans"<<ans;
 return 0;
 }
