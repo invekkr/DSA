947960b28e0efd6d72c951f13fa8c38d8e992138
invek <invekkr@gmail.com>
2025-05-14 19:53:19 +0530

    insert at position

--- Linked List/insertionLL.cpp
+++ Linked List/insertionLL.cpp
@@ -37,22 +37,46 @@ Node* insertTail(Node* &head, int val){
     }
     temp->next = x;
     return head;
+}
 
+Node* insertPos(Node* &head, int val, int pos){
+    if(pos<=0){
+        cout<<"invalid position ";
+    }
+    if(pos==1){
+        return insertHead(head,val);
+    }
+
+    Node* temp = head;
+    int cnt = 0;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt==pos-1){
+            Node *x = new Node(val);
+            x->next = temp->next;
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
 }
 
 int main(){
-    Node* first = NULL;
-    // Node* first = new Node(1);
-    // Node* second = new Node(2);
-    // Node* third = new Node(3);
-    // Node* fourth = new Node(4);
-    // first->next = second;
-    // second->next = third;
-    // third->next = fourth;
+    
+    Node* first = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
     int val1 = 89;
     int val2 = 99;
+    int pos = 0,val3 = 100;
     printLL(first);
-    first = insertHead(first,val1);
-    first = insertTail(first,val2);
+    // first = insertHead(first,val1);
+    // first = insertTail(first,val2);
+    first = insertPos(first,val3,pos);
     printLL(first);
 }
