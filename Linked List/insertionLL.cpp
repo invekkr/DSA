c304eb23b436f0754de5109a5db501334ce053a1
invek <invekkr@gmail.com>
2025-05-14 19:05:30 +0530

    insert at ehad and tail

--- Linked List/insertionLL.cpp
+++ Linked List/insertionLL.cpp
@@ -12,20 +12,47 @@ class Node{
 };
 void printLL(Node* head){
     Node* temp = head;
+    cout<<"LL : ";
     while(temp){
         cout<<temp->data<<" ";
         temp = temp->next;
     }
     cout<<endl;
 }
-int main(){
-    Node* first = new Node(1);
-    Node* second = new Node(2);
-    Node* third = new Node(3);
-    Node* fourth = new Node(4);
-    first->next = second;
-    second->next = third;
-    third->next = fourth;
+Node* insertHead(Node* &head, int val){
+    Node* temp = new Node(val);
+    temp->next = head;
+    head = temp;
+    return head;
 
+}
+Node* insertTail(Node* &head, int val){
+    Node* temp = head, *x = new Node(val);
+    if(head==NULL){
+        head = x;
+        return head;
+    }
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = x;
+    return head;
+
+}
+
+int main(){
+    Node* first = NULL;
+    // Node* first = new Node(1);
+    // Node* second = new Node(2);
+    // Node* third = new Node(3);
+    // Node* fourth = new Node(4);
+    // first->next = second;
+    // second->next = third;
+    // third->next = fourth;
+    int val1 = 89;
+    int val2 = 99;
+    printLL(first);
+    first = insertHead(first,val1);
+    first = insertTail(first,val2);
     printLL(first);
 }
