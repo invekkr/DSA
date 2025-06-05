74a1c569c07eb6e3c12230fc63335726567ae6bd
invek <invekkr@gmail.com>
2025-06-05 22:53:32 +0530

    combination sum 2 using looping

--- Recursion/10_combinationSum2.cpp
+++ Recursion/10_combinationSum2.cpp
@@ -1,35 +1,27 @@
+// https://leetcode.com/problems/combination-sum-ii/description/
+
 #include <bits/stdc++.h>
 using namespace std;
-void comb(int i, vector<vector<int>>&ans, vector<int>&li, vector<int>&arr, int t){
-        if(i==arr.size()){
-            if(t==0){
-                ans.push_back(li);
-            }return;
+void comb(int ind, vector<vector<int>> &ans, vector<int>&li,vector<int>& arr, int t){
+        if(t==0){
+            ans.push_back(li);
+            return;
         }
-        if(arr[i]<=t){
+        for(int i=ind;i<arr.size();i++){
+            if(i>ind && arr[i]==arr[i-1])   continue;   // skips dupes
+            if(arr[i]>t)    break;
             li.push_back(arr[i]);
             comb(i+1,ans,li,arr,t-arr[i]);
-            li.pop_back();
+            li.pop_back();    
         }
-        comb(i+1,ans,li,arr,t);
     }
-vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
-{
-    vector<vector<int>> ans;
-    vector<int> li;
-    comb(0, ans, li, candidates, target);
-    sort(ans.begin(), ans.end()); // Optional but good for consistent ordering
-
-    set<vector<int>> uniqueRows;
-    for (auto row : ans)
-    {
-        sort(row.begin(), row.end()); // ✅ Sort each combination before inserting into the set
-        uniqueRows.insert(row);
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> li;
+        comb(0,ans,li,candidates,target);
+        return ans;
     }
-
-    vector<vector<int>> result(uniqueRows.begin(), uniqueRows.end());
-    return result;
-}
 void printVectorOfVector(const vector<vector<int>>& vec) {
     for (const auto& inner : vec) {
         cout << "[ ";
