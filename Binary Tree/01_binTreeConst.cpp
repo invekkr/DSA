82c5d84a9485c6ded550b4b6e81158eaaf3a10c9
invek <invekkr@gmail.com>
2025-08-20 23:37:21 +0530

    bin tree const

--- Binary Tree/01_binTreeConst.cpp
+++ Binary Tree/01_binTreeConst.cpp
@@ -12,16 +12,34 @@ class Node{
     }
 
 };
+Node* buildTree(Node* root){
+    cout<<"Enter value for Node : "<<endl;
+    int x;
+    cin>>x;
+    if(x==-1)   return NULL;
+
+    root = new Node(x);
+
+    cout<<"Enter data to the left of "<<x<<endl;
+    root->left = buildTree(root->left);
+    
+    cout<<"Enter data to the right of "<<x<<endl;
+    root->right = buildTree(root->right);
+    
+    return root;
+
+}
+
 void inOrder(Node* head){
     if(head == NULL)    return;
     inOrder(head->left);
-    cout<<head->data;
+    cout<<head->data<<" ";
     inOrder(head->right);
 
 }
 void preOrder(Node* head){
     if(head==NULL)  return;
-    cout<<head->data;
+    cout<<head->data<<" ";
     preOrder(head->left);
     preOrder(head->right);
 }
@@ -29,12 +47,12 @@ void postOrder(Node* head){
     if(head==NULL)  return;
     postOrder(head->left);
     postOrder(head->right);
-    cout<<head->data;
+    cout<<head->data<<" ";
 }
 int main(){
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
+
+    Node* root = NULL;
+    root = buildTree(root);
     inOrder(root);
     cout<<endl;
     preOrder(root);
