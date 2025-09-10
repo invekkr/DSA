7bf9b8b526545c0ffbf49c19d4d494e724e4e755
invek <invekkr@gmail.com>
2025-09-10 10:06:45 +0530

    Iterartive preorder

---
+++ Tree/Binary Tree/12_cousionOrNot.cpp
@@ -0,0 +1,1 @@
+// https://www.geeksforgeeks.org/problems/check-if-two-nodes-are-cousins/1
---
+++ Tree/Binary Tree/13_iterativeTraversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        stack<TreeNode *> st;
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        st.push(root);
+        while (!st.empty())
+        {
+            TreeNode *temp = st.top();
+            ans.push_back(temp->val);
+            st.pop();
+            if (temp->right)
+                st.push(root->right);
+            if (temp->left)
+                st.push(root->left);
+        }
+        return ans;
+    }
+};
+int main()
+{
+
+    return 0;
+}
