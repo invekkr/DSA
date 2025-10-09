2683a49ddf3286c50319032bd45feed963734140
invek <invekkr@gmail.com>
2025-10-09 19:45:01 +0530

    Subsequence using loops

--- Recursion/04_subsequences.cpp
+++ Recursion/04_subsequences.cpp
@@ -1,26 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void subSeq(int ind, vector<int> &list, int arr[],int n){
-    
-    if(ind>=n){
-        for(auto it : list){
-        cout<<it<<" ";
+
+void subSeq(int start, vector<int> &list, int arr[], int n) {
+    // print current subset
+    for (auto it : list) {
+        cout << it << " ";
     }
-    cout<<endl;
-    return;
+    cout << endl;
+
+    // loop through all possible choices from 'start'
+    for (int i = start; i < n; i++) {
+        list.push_back(arr[i]);            // choose arr[i]
+        subSeq(i + 1, list, arr, n);       // recurse for next index
+        list.pop_back();                   // backtrack (undo choice)
     }
-    list.push_back(arr[ind]);
-    subSeq(ind+1, list,arr,n);
-    list.pop_back();
-    subSeq(ind+1, list,arr,n);
 }
 
-int main(){
-    int arr[] = {1,2,3};
+int main() {
+    int arr[] = {3, 2, 1};
     int n = 3;
     vector<int> list;
-    subSeq(0,list,arr,n);
-    cout<<"SubSequences printed";
+    subSeq(0, list, arr, n);
+    cout << "SubSequences printed";
     return 0;
-
 }
