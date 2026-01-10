47d327660f020eb1214dc49dbf4fe2638246d51b
invek <youremail@gmail.com>
2026-01-11 00:22:27 +0530

    max SubArray Sum with size K

--- Linked List/07_searchLL.cpp
+++ Linked List/07_searchLL.cpp
@@ -28,10 +28,10 @@ bool searchLL(Node* head, int d){
     return false;
 }
 int main(){
-    Node* first = new Node(1);
+    Node* first = new Node(101);
     Node* second = new Node(2);
-    Node* third = new Node(3);
-    Node* fourth = new Node(4);
+    Node* third = new Node(4);
+    Node* fourth = new Node(84);
     first->next = second;
     second->next = third;
     third->next = fourth;
@@ -39,7 +39,7 @@ int main(){
     printLL(first);
     cout<<endl;
 
-    bool ans = searchLL(first, 1);
+    bool ans = searchLL(first, 0);
     cout<<ans;
 return 0;
 }
